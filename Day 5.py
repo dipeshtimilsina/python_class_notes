e0abbad570106f8ec1f669a4fd427009e62d2956
Bijesh Raj Kunwar <bijeshr.kunwar@gmail.com>
2019-11-11 07:13:00 +0545

    Add files via upload

---
+++ Day 5.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ### While Loop
+
+# In[3]:
+
+
+i=0
+while i<5:
+    print(i)
+    i += 1
+
+
+# In[4]:
+
+
+i = 0
+while True:
+    print(i)
+    i+=1
+    if i == 5:
+        break
+
+
+# In[ ]:
+
+
+
+
